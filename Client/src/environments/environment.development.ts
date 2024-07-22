828fad2528a09f9059331b0aded274cda3a4cf5e
Sasanka <myaswanth34@gmail.com>
2024-07-22 14:56:26 +0400

    before laptop change

--- Client/src/environments/environment.development.ts
+++ Client/src/environments/environment.development.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:7212/api/', //original URL
+  //*** M test ***/
   //apiUrl: 'https://mtest.dnrd.ae:8443/InnovationProxyGateway/gateway/',
+
+  //***  Localhost ***/
   apiUrl: 'https://localhost:7010/gateway/',
+
+  //*** Live Production ***/
+  //apiUrl: 'https://m.dnrd.ae:443/InnovationProxyGateway/gateway/',
 };
